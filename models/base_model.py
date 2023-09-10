330675331944c8835c4bbe4611764f35c72d07dd
jmukamani <j.mukamani@alustudent.com>
2023-09-10 16:50:40 +0300

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -15,6 +15,7 @@ class BaseModel:
         tformat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
+        self.updated_at = self.created_at
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key  == "created_at" or key == "updated_at":
